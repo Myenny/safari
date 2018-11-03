bfe7afba76ad0859209eee99b114349482db02a3
ooeygooeygold <amichaelj1@gmail.com>
2018-11-03 18:14:13 -0400

    Connecting front to backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Navbar,
+  NavbarBrand
+} from 'reactstrap'
 
 class App extends Component {
+  state = {
+    SeenAnimals: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/SafariVacation/')
+      .then(resp => resp.json())
+      .then(SeenAnimals => {
+        this.setState({ SeenAnimals })
+      })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar color="dark" light expand="md">
+          <NavbarBrand href="/" color="dark">
+            {' '}
+            Jungle
+          </NavbarBrand>
+        </Navbar>
+        <ul>
+          {this.state.SeenAnimals.map(animal => {
+            return (
+              <li>
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>
+                      {animal.species} {animal.countOfTimesSeen}{' '}
+                      {animal.locationOfLastSeen}
+                    </CardTitle>
+                  </CardBody>
+                </Card>
+              </li>
+            )
+          })}
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
