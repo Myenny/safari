3ab0f5ac9d92d9a5b381a6b8cba20397bf0528f8
ooeygooeygold <amichaelj1@gmail.com>
2018-11-13 09:34:07 -0500

    “gg”

--- src/App.js
+++ src/App.js
@@ -19,41 +19,31 @@ class App extends Component {
     fetch('http://localhost:5000/api/SafariVacation/')
       .then(resp => resp.json())
       .then(SeenAnimals => {
+        console.log(SeenAnimals)
         this.setState({ SeenAnimals })
       })
   }
   render() {
     return (
-      <div className="App">
-        <Navbar color="dark" light expand="md">
-          <NavbarBrand href="/" color="dark">
-            {' '}
-            Jungle
-          </NavbarBrand>
-        </Navbar>
-        <ul>
-          {this.state.SeenAnimals.map(animal => {
-            return (
-              <li>
-                <Card>
-                  <CardImg
-                    top
-                    width="100%"
-                    src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                    alt="Card image cap"
-                  />
-                  <CardBody>
-                    <CardTitle>
-                      {animal.species} {animal.countOfTimesSeen}{' '}
-                      {animal.locationOfLastSeen}
-                    </CardTitle>
-                  </CardBody>
-                </Card>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <ul>
+        {this.state.SeenAnimals.map(animal => (
+          <li>
+            <Card>
+              <CardBody>
+                <CardTitle> Animal Name: {animal.species}</CardTitle>
+                <CardSubtitle>
+                  Location:
+                  {animal.locationOfLastSeen}
+                </CardSubtitle>
+                <CardSubtitle>
+                  Times Seen:
+                  {animal.countOfTimesSeen}
+                </CardSubtitle>
+              </CardBody>
+            </Card>
+          </li>
+        ))}
+      </ul>
     )
   }
 }
